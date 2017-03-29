824913a41966557dff6490892101a4a0d42debf4
Eric Praline <Eric Praline>
2017-03-29 14:38:56 +0300

    changed comments

--- 1_pyth.h
+++ 1_pyth.h
@@ -1,6 +1,6 @@
 #pragma once
 
-bool isPythagoreanTriple(int x, int y, int z) //returns 1 if set of ints is Pythagorean triple
+bool isPythagoreanTriple(int x, int y, int z) //returns true if set of ints is Pythagorean triple
 {
 	if ((x * x + y * y == z * z) || (x * x + z * z == y * y) || (y * y + z * z == x * x)) {
 		return true;
