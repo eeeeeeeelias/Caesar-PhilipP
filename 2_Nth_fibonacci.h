ebb391bc4c550fdd8925042f4e2faa34f27e026d
Eric Praline <Eric Praline>
2017-03-29 13:22:09 +0300

    some error

---
+++ 2_Nth_fibonacci.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <algorithm>
+
+struct intpair
+{
+	int first = 0;
+	int second = 0;
+};
+
+intpair Nth_Fibonacci(int N) //returns ( f(N-1), f(N) )
+{ 
+	intpair MyPair;
+	MyPair.first = 0;
+	MyPair.second = 1;
+	for (int i = 2; i <= N; i++) {
+		MyPair.first += MyPair.second;
+		std::swap(MyPair.first, MyPair.second);
+	}
+	return MyPair;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
+#pragma once
+
+#include "1_pythagoreantriples.h"
+#include "2_Nth_fibonacci.h"
 #include <iostream>
 
-int main() {
+int main() 
+{
+	int x = 0, y = 0, z = 0;
+	std::cout << "Insert 3 integers" << std::endl;
+	std::cin >> x >> y >> z;
+/*	if (isPythagoreanTriple(x, y, z)) {
+		std::cout << "YES" << std::endl;
+	}
+	else {
+		std::cout << "NO" << std::endl;
+	}*/
 }
