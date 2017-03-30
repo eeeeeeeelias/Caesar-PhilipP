1e12222e1aff3af566ea8b394b046a783ace6730
Eric Praline <Eric Praline>
2017-03-30 16:43:09 +0300

    now there are 2 Levenshteins, no useless files

--- C-P-Week2/3_levenshtein.h
+++ C-P-Week2/3_levenshtein.h
@@ -3,11 +3,12 @@
 #include <algorithm>
 const int MAX_LENGTH = 100;
 
-int levenshtein(std::string a, std::string b, int matrix[][MAX_LENGTH]) //time complexity = O( a.size() * b.size() ), space complexity = O( a.size() * b.size() )
+int memorizingLevenshtein(std::string a, std::string b, int matrix[][MAX_LENGTH], int insertCost, int deleteCost, int replaceCost) 
+//time complexity = O( a.size() * b.size() ), space complexity = O( a.size() * b.size() )
 {
-	int insertCost = 1, deleteCost = 1, replaceCost = 1;
 	a = " " + a;
 	b = " " + b;
+	//start matrix filling
 	matrix[0][0] = 0;
 	for (int j = 1; j < b.size(); j++) {
 		matrix[0][j] = matrix[0][j - 1] + insertCost;
@@ -16,8 +17,37 @@ int levenshtein(std::string a, std::string b, int matrix[][MAX_LENGTH]) //time c
 		matrix[i][0] = matrix[i - 1][0] + deleteCost;
 		for (int j = 1; j <= b.size(); j++) {
 			matrix[i][j] = std::min(matrix[i - 1][j] + deleteCost,
-				matrix[i][j - 1] + insertCost, matrix[i - 1][j - 1] + replaceCost);
+				std::min(matrix[i][j - 1] + insertCost, matrix[i - 1][j - 1] + ((a[i] == b[j]) ? 0 : replaceCost)));
 		}
 	}
 	return matrix[a.size() - 1][b.size() - 1];
+}
+
+int obliviousLevenshtein(std::string a, std::string b, int insertCost, int deleteCost, int replaceCost) 
+// time complexity = O( a.size() * b.size() ), space complexity = O( min(a, b).size() )
+{
+	int matrix[2][MAX_LENGTH];
+	if (a.size() < b.size()) {
+		a.swap(b);
+	}
+	a = " " + a;
+	b = " " + b;
+	//start matrix filling
+	matrix[0][0] = 0;
+	for (int j = 1; j < b.size(); j++) {
+		matrix[0][j] = matrix[0][j - 1] + insertCost;
+	}
+	for (int i = 1; i < a.size(); i++) {
+		matrix[1][0] = matrix[0][0] + deleteCost;
+		for (int j = 1; j < b.size(); j++) {
+			matrix[1][j] = std::min(matrix[0][j] + deleteCost, 
+				std::min(matrix[1][j - 1] + insertCost, matrix[0][j - 1] + ((a[i] == b[j]) ? 0 : replaceCost)));
+		}
+		for (int j = 0; j < b.size(); j++) {
+			matrix[0][j] = matrix[0][j] ^ matrix[1][j];
+			matrix[1][j] = matrix[0][j] ^ matrix[1][j];
+			matrix[0][j] = matrix[0][j] ^ matrix[1][j];
+		}
+	}
+	return matrix[0][b.size() - 1];
 }
--- C-P-Week2/C-P-Week2.cpp
+++ C-P-Week2/C-P-Week2.cpp
@@ -3,13 +3,15 @@
 
 int main()
 {
-	int matrix[MAX_LENGTH][MAX_LENGTH];
+//	int matrix[MAX_LENGTH][MAX_LENGTH];
 	std::string a = "", b = "";
-	std::cout << "3. Levenstein distance";
+	std::cout << "3. Levenstein distance" << std::endl;
 	std::cout << "Insert 1st string: ";
 	std::cin >> a;
 	std::cout << "Insert 2nd string: ";
 	std::cin >> b;
-	int dist = levenshtein(a, b, matrix);
+//	std::cout << "distance(\"" << a << "\", \"" << b << "\") = " << memorizingLevenshtein(a, b, matrix, 1, 1, 1) << std::endl;
+	std::cout << "distance(\"" << a << "\", \"" << b << "\") = " << obliviousLevenshtein(a, b, 1, 1, 1) << std::endl;
+	system("pause");
 	return 0;
 }
