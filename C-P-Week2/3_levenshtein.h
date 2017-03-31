010dd50245f7c5ba15b4bad04d8ff3f1f2f34e9e
Eric Praline <Eric Praline>
2017-03-31 22:59:54 +0300

    normal swap, no more signed/unsigned warnings

--- C-P-Week2/3_levenshtein.h
+++ C-P-Week2/3_levenshtein.h
@@ -8,19 +8,20 @@ int memorizingLevenshtein(std::string a, std::string b, int matrix[][MAX_LENGTH]
 {
 	a = " " + a;
 	b = " " + b;
+	int aSize = (int)a.size(), bSize = (int)b.size();
 	//start matrix filling
 	matrix[0][0] = 0;
-	for (int j = 1; j < b.size(); j++) {
+	for (int j = 1; j < bSize; j++) {
 		matrix[0][j] = matrix[0][j - 1] + insertCost;
 	}
-	for (int i = 1; i < a.size(); i++) {
+	for (int i = 1; i < aSize; i++) {
 		matrix[i][0] = matrix[i - 1][0] + deleteCost;
-		for (int j = 1; j <= b.size(); j++) {
+		for (int j = 1; j <= bSize; j++) {
 			matrix[i][j] = std::min(matrix[i - 1][j] + deleteCost,
 				std::min(matrix[i][j - 1] + insertCost, matrix[i - 1][j - 1] + ((a[i] == b[j]) ? 0 : replaceCost)));
 		}
 	}
-	return matrix[a.size() - 1][b.size() - 1];
+	return matrix[aSize - 1][bSize - 1];
 }
 
 int obliviousLevenshtein(std::string a, std::string b, int insertCost, int deleteCost, int replaceCost) 
@@ -32,22 +33,21 @@ int obliviousLevenshtein(std::string a, std::string b, int insertCost, int delet
 	}
 	a = " " + a;
 	b = " " + b;
+	int aSize = (int)a.size(), bSize = (int)b.size();
 	//start matrix filling
 	matrix[0][0] = 0;
-	for (int j = 1; j < b.size(); j++) {
+	for (int j = 1; j < bSize; j++) {
 		matrix[0][j] = matrix[0][j - 1] + insertCost;
 	}
-	for (int i = 1; i < a.size(); i++) {
+	for (int i = 1; i < aSize; i++) {
 		matrix[1][0] = matrix[0][0] + deleteCost;
-		for (int j = 1; j < b.size(); j++) {
+		for (int j = 1; j < bSize; j++) {
 			matrix[1][j] = std::min(matrix[0][j] + deleteCost, 
 				std::min(matrix[1][j - 1] + insertCost, matrix[0][j - 1] + ((a[i] == b[j]) ? 0 : replaceCost)));
 		}
-		for (int j = 0; j < b.size(); j++) {
-			matrix[0][j] = matrix[0][j] ^ matrix[1][j];
-			matrix[1][j] = matrix[0][j] ^ matrix[1][j];
-			matrix[0][j] = matrix[0][j] ^ matrix[1][j];
+		for (int j = 0; j < bSize; j++) {
+			std::swap(matrix[0][j], matrix[1][j]);
 		}
 	}
-	return matrix[0][b.size() - 1];
+	return matrix[0][bSize - 1];
 }
